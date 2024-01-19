005b0c5ac3993602dd67985082b8daee554ffd07
meo <83820503+meo-pill@users.noreply.github.com>
2024-01-19 16:27:00 +0100

    ajout de la création du serveur

--- com/morpion/Server.java
+++ com/morpion/Server.java
@@ -1,5 +1,34 @@
 package com.morpion;
 
+import com.morpion.game.Morpion;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+/**
+ * Instanciation du serveur
+ * 
+ * @author @meo-pill
+ * @see com.morpion.game.Morpion
+ */
 public class Server {
-    
+    public Server() throws RemoteException {
+        System.out.println("serveur crée");
+        try {
+            LocateRegistry.createRegistry(1099);
+            Morpion morpion = new Morpion();
+            Naming.rebind("rmi:///Morpion", morpion);
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            System.out.println("Serveur prêt");
+        }
+    }
+
+    public static void main(String[] args) throws RemoteException {
+        System.out.println("creation du serveur");
+        new Server();
+        System.out.println("Serveur arrêté");
+    }
 }
---
+++ com/morpion/game/Morpion.java
@@ -0,0 +1,10 @@
+package com.morpion.game;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Morpion extends UnicastRemoteObject {
+    public Morpion() throws RemoteException {
+        super();
+    }
+}
